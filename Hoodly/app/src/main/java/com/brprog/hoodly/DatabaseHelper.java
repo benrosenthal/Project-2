8f9d45b33e43224e62ba9bec1ef17f809d5207c3
Ben <benrosenthal664@gmail.com>
2016-02-08 11:34:13 -0500

    Third Commit Setting TextView From Helpers misnamed  GetNameByDescription method

--- Hoodly/app/src/main/java/com/brprog/hoodly/DatabaseHelper.java
+++ Hoodly/app/src/main/java/com/brprog/hoodly/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.brprog.hoodly;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -10,30 +11,56 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    private static final String TAG = DatabaseHelper.class.getCanonicalName();
+
+    //03 Singleton
+    private static DatabaseHelper mInstance;
+
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "Hood.db";
     public static final String TABLE_NAME = "Hoods";
+    public static final String COL_ID = "_id";
+    public static final String COL_ITEM_NAME = "ITEM_NAME";
+    public static final String COL_ITEM_ADDRESS = "ITEM_ADDRESS";
+    public static final String COL_ITEM_DESCRIPTION = "ITEM_DESCRIPTION";
+    public static final String COL_IS_FAVORITE = "IS_FAVORITE";
 
+    public static final String[] HOODLY_COLUMNS = {COL_ID, COL_ITEM_NAME, COL_ITEM_ADDRESS, COL_ITEM_DESCRIPTION, COL_IS_FAVORITE};
     // Define sql statements to create and delete games table
     public static final String SQL_CREATE_HOOD_TABLE =
-            "CREATE TABLE Hoods ( id INTEGER PRIMARY KEY, name TEXT, address TEXT, favorite INTEGER )";
+            "CREATE TABLE " + TABLE_NAME +
+                    "(" +
+                    COL_ID + " INTEGER PRIMARY KEY, " +
+                    COL_ITEM_NAME + " TEXT, " +
+                    COL_ITEM_ADDRESS + " TEXT, " +
+                    COL_ITEM_DESCRIPTION + " TEXT, " +
+                    COL_IS_FAVORITE + " INTEGER )";
 
+    //KEY AUTOINCREMENT
     public static final String SQL_DROP_HOOD_TABLE = "DROP TABLE IF EXISTS games";
 
 
-    public DatabaseHelper(Context context) {
+    private DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    
+//03 Singleton -- private constructor 
+    public static DatabaseHelper getInstance(Context context){
+        if(mInstance == null){
+            mInstance = new DatabaseHelper(context.getApplicationContext());
+        }
+        return mInstance;
     }
 
     // Create the games table when the database is created
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_HOOD_TABLE);
-        insertHood(db,1, "Big Building", "123 Street", 0);
-        insertHood(db, 2, "Medium Building", "456 Street", 0);
-        insertHood(db, 3, "Large Building", "789 Street", 0);
-        insertHood(db, 4, "Small Park", "123 Avenue", 0);
-        insertHood(db, 5, "Medium Park", "456 Avenue", 0);
-        insertHood(db, 6, "Large Park", "789 Avenue", 0);
+        insertHood(db,1, "Big Building", "123 Street", "Building", 0);
+        insertHood(db, 2, "Medium Building", "456 Street", "Building", 0);
+        insertHood(db, 3, "Large Building", "789 Street", "Building", 0);
+        insertHood(db, 4, "Small Park", "123 Avenue", "Park", 0);
+        insertHood(db, 5, "Medium Park", "456 Avenue", "Park", 0);
+        insertHood(db, 6, "Large Park", "789 Avenue", "Park", 0);
     }
 
     // When the database is upgraded, the old data isn't needed. Delete the games
@@ -43,21 +70,46 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public void insertHood(SQLiteDatabase db, int id, String name, String address, int favorite){
+    public void insertHood(SQLiteDatabase db, int id, String name, String address, String description, int favorite){
         // Get a reference to the database
         //SQLiteDatabase db = getWritableDatabase();
 
         // create a new content value to store values
         ContentValues values = new ContentValues();
-        values.put("id", id);
-        values.put("name", name);
-        values.put("address", address);
-        values.put("favorite", favorite);
+        values.put(COL_ID, id);
+        values.put(COL_ITEM_NAME, name);
+        values.put(COL_ITEM_ADDRESS, address);
+        values.put(COL_ITEM_DESCRIPTION, description);
+        values.put(COL_IS_FAVORITE, favorite);
+
 
         // Insert the row into the games table
         db.insert(TABLE_NAME, null, values);
     }
 
+    public String getDescriptionByName(String name){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME,
+                null,
+                COL_ITEM_NAME+" = ?",
+                new String[] {name},
+                null,
+                null,
+                null);
+
+
+
+
+        if(cursor.moveToFirst()){
+            //String resultVal = cursor.getString(cursor.getColumnIndex(COL_ID));
+            String resultVal = cursor.getString(cursor.getColumnIndex(COL_ITEM_NAME));
+            return resultVal;
+            //return cursor.getString()
+        } else {
+            return "No Description Found";
+        }
+    }
 //    public Hood getHood(int id){
 //        // Get a reference to the database
 //        SQLiteDatabase db = getReadableDatabase();
@@ -98,7 +150,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         String[] selectionArgs = new String[]{ String.valueOf(id) };
 
         // Delete everything that satisfies the selection
-        db.delete("games", selection, selectionArgs);
+        db.delete("Hoods", selection, selectionArgs);
     }
 
 
--- Hoodly/app/src/main/java/com/brprog/hoodly/MainActivity.java
+++ Hoodly/app/src/main/java/com/brprog/hoodly/MainActivity.java
@@ -2,8 +2,6 @@ package com.brprog.hoodly;
 
 import android.app.SearchManager;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
@@ -15,7 +13,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    SQLiteDatabase mDb;
+    DatabaseHelper mHoodlyHelper;
     TextView mTextView;
 
 
@@ -26,13 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
         mTextView = (TextView)findViewById(R.id.count_text_view);
 
-        DatabaseHelper hoodlyHelper = new DatabaseHelper(this);
-        mDb = hoodlyHelper.getReadableDatabase();
-        Cursor cursor = mDb.query("Hoods", new String[] {"name"}, null, null, null, null, null);
-
-        mTextView.setText(cursor.getColumnName(0));
-        handleIntent(getIntent());
-        cursor.close();
+        DatabaseHelper mHoodlyHelper = DatabaseHelper.getInstance(MainActivity.this);
+        //Cursor cursor = mDb.query("Hoods", new String[] {"name"}, null, null, null, null, null);
+       //mTextView.setText(hoodlyHelper.getDescriptionById(1));
+        //mTextView.setText(cursor.getColumnName(0));
+        mTextView.setText(mHoodlyHelper.getDescriptionByName("Big Building"));
+        //handleIntent(getIntent());
     }
 
     @Override
@@ -57,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
+            mHoodlyHelper.getDescriptionByName(query);
             Toast.makeText(MainActivity.this, "Searching for " + query, Toast.LENGTH_SHORT).show();
         }
     }
